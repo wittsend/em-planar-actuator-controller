23fe528d395b6f932b28fb63c43ac514c9906084
Matthew Witt <wittsend86@gmail.com>
2017-08-03 19:18:13 +1200

    Create project files. Establish .gitignore

---
+++ empac/empac/main.c
@@ -0,0 +1,18 @@
+/*
+ * empac.c
+ *
+ * Created: 3/08/2017 7:16:27 PM
+ * Author : Fiona
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
