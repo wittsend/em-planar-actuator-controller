21969814ed7b90bf68f140afa2cafc616c0872cc
Matthew Witt <wittsend86@gmail.com>
2017-08-03 20:29:23 +1200

    Create formatting and layout for pio

--- empac/pio.c
+++ empac/pio.c
@@ -1,10 +1,45 @@
 /*
- * pio.c
- *
- * Created: 3/08/2017 7:31:46 PM
- *  Author: Fiona
- */ 
+* pio.c
+*
+* Author : Matthew Witt (pxf5695@autuni.ac.nz)
+* Created: 3/08/2017 7:45:54 PM
+*
+* Project Repository:https://github.com/wittsend/em-planar-actuator-controller
+*
+* Contains parallel IO initialisation routine
+*
+* More Info:
+* Atmel AT90USB1287 Datasheet:http://www.atmel.com/images/doc7593.pdf
+* Relevant reference materials or datasheets if applicable
+*
+* Functions:
+* void pioInit(void)
+*
+*/
 
+///////////////Includes/////////////////////////////////////////////////////////////////////////////
+#include "pio.h"
+
+///////////////Defines//////////////////////////////////////////////////////////////////////////////
+
+///////////////Functions////////////////////////////////////////////////////////////////////////////
+/*
+* Function:
+* void pioInit(void)
+*
+* Initialises IO pins on the microcontroller to perform their tasks.
+*
+* Inputs:
+* none
+*
+* Returns:
+* none
+*
+* Implementation:
+* TODO:[explain key steps of function]
+* [use heavy detail for anything complicated]
+*
+*/
 void pioInit(void)
 {
 //Lab board IO mux control.
--- empac/pio.h
+++ empac/pio.h
@@ -1,16 +1,43 @@
 /*
- * pio.h
- *
- * Created: 3/08/2017 7:45:54 PM
- *  Author: Fiona
- */ 
-
+* pio.h
+*
+* Author : Matthew Witt (pxf5695@autuni.ac.nz)
+* Created: 3/08/2017 7:45:54 PM
+*
+* Project Repository:https://github.com/wittsend/em-planar-actuator-controller
+*
+* Contains pin definitions and parallel IO initialisation function prototype
+*
+* More Info:
+* Atmel AT90USB1287 Datasheet:http://www.atmel.com/images/doc7593.pdf
+* Relevant reference materials or datasheets if applicable
+*
+* Functions:
+* void pioInit(void)
+*
+*/
 
 #ifndef PIO_H_
 #define PIO_H_
+///////////////Includes/////////////////////////////////////////////////////////////////////////////
 
+///////////////Defines//////////////////////////////////////////////////////////////////////////////
 
-
+///////////////Functions////////////////////////////////////////////////////////////////////////////
+/*
+* Function:
+* void pioInit(void)
+*
+* Initialises IO pins on the microcontroller to perform their tasks.
+*
+* Inputs:
+* none
+*
+* Returns:
+* none
+*
+*/
+void pioInit(void);
 
 
 #endif /* PIO_H_ */
